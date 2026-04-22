e3f219fd8a60eee026c28472c8430d1ac8a3cfbd
Breakcrumps <sonickita7@gmail.com>
2026-04-22 15:38:14 +0300

    Good stuff

--- Gun.cs
+++ Gun.cs
@@ -10,19 +10,36 @@ internal sealed partial class Gun : Node3D
 
   [Export] private Node3D _rightLeanPivot = null!;
   [Export] private Node3D _leftLeanPivot = null!;
-  private Vector3 _defaultPos;
+  private Vector3 _initPos;
   
   [Export] private float _leanLeftAngle = Mathf.Pi / 10f;
   [Export] private float _leanRightAngle = -Mathf.Pi / 10f;
   [Export] private float _leanSpeed = 10f;
 
-  internal Vector3 ExtPosOffset { private get; set; }
-  internal Vector3 ExtRotOffset { get; set; }
+  [Export] private Node3D _nearWallState = null!;
+  [Export] private RayCast3D _gunRay = null!;
+
+  private Vector3 _rotOffset;
+  private Vector3 _swayOffset;
+  private Vector3 _wallOffset;
+
+  [ExportGroup("Weapon Sway")]
+  [Export] private float _swayAmount = 0.5f;
+  [Export] private float _swayThreshold = 0.05f;
+  [Export] private float _swayLerpSpeed = 10f;
+  [Export] private float _pullBackSpeed = 15f;
+  private Vector2 _mouseRelative;
 
   public override void _Ready()
   {
-    _defaultPos = Position;
+    _initPos = Position;
     _bulletSpawner.Camera = _camera;
+  }
+
+  public override void _UnhandledInput(InputEvent @event)
+  {
+    if (@event is InputEventMouseMotion mouseMotion)
+      _mouseRelative = mouseMotion.Relative;
   }
 
   public override void _PhysicsProcess(double delta)
@@ -31,6 +48,50 @@ internal sealed partial class Gun : Node3D
       Position = Position.Lerp(to: _aimPivot.Position, weight: _leanSpeed * (float)delta);
     else
       HandleLean(delta);
+  }
+
+  private void UpdateNearWallOffsets(double delta)
+  {
+    Vector3 targetPosOffset = Vector3.Zero, targetOrientation = Vector3.Zero;
+    float maxDist = _gunRay.TargetPosition.Length(); 
+
+    if (_gunRay.IsColliding())
+    {
+      float collisionDist = _gunRay.GetCollisionPoint().DistanceTo(_gunRay.GlobalPosition);
+      float proximity = Mathf.Clamp(1.0f - (collisionDist / maxDist), 0f, 1f);
+      
+      targetPosOffset = proximity * (_nearWallState.Position - _initPos);
+      targetOrientation = proximity * _nearWallState.Rotation;
+    }
+
+    // _gun.Position = _gun.Position.Lerp(to: targetPos, weight: _pullBackSpeed * (float)delta);
+    _wallOffset = _wallOffset.Lerp(to: targetPosOffset, weight: _pullBackSpeed * (float)delta);
+    // _gun.Rotation = _gun.Rotation.Lerp(to: targetOrientation, weight: _pullBackSpeed * (float)delta);
+    _rotOffset = _rotOffset.Lerp(to: targetOrientation, weight: _pullBackSpeed * (float)delta);
+  }
+
+  private void UpdateSwayOffsets(double delta)
+  {
+    if (_mouseRelative.LengthSquared() < .5f)
+    {
+      _mouseRelative = Vector2.Zero;
+      return;
+    }
+    else if ((_mouseRelative.X > 0 && _swayOffset.X > 0) || (_mouseRelative.X < 0 && _swayOffset.X < 0))
+      _mouseRelative.X = 0;
+    
+    float targetSwayX = -_mouseRelative.X * _swayAmount;
+    float targetSwayY = _mouseRelative.Y * _swayAmount;
+
+    targetSwayX = Mathf.Clamp(targetSwayX, -_swayThreshold, _swayThreshold);
+    targetSwayY = Mathf.Clamp(targetSwayY, -_swayThreshold, _swayThreshold);
+
+    Vector3 targetSway = new(targetSwayX, targetSwayY, 0);
+
+    // _gun.Position = _gun.Position.Lerp(_gunDefaultPos + swayPos, _swayLerpSpeed * (float)delta);
+    _swayOffset = _swayOffset.Lerp(targetSway, _swayLerpSpeed * (float)delta);
+
+    _mouseRelative *= .2f;
   }
 
   private void HandleLean(double delta)
@@ -53,12 +114,15 @@ internal sealed partial class Gun : Node3D
     }
     else
     {
-      nextPos = _defaultPos;
+      nextPos = _initPos;
       nextRot = Quaternion.Identity;
     }
 
-    nextPos += ExtPosOffset;
-    nextRot *= Quaternion.FromEuler(ExtRotOffset);
+    UpdateNearWallOffsets(delta);
+    UpdateSwayOffsets(delta);
+
+    nextPos += _wallOffset + _swayOffset;
+    nextRot *= Quaternion.FromEuler(_rotOffset);
 
     Position = Position.Lerp(to: nextPos, weight: _leanSpeed * (float)delta);
     Quaternion = Quaternion.Slerp(to: nextRot, weight: _leanSpeed * (float)delta);
--- PetraCamera.cs
+++ PetraCamera.cs
@@ -5,11 +5,6 @@ internal sealed partial class PetraCamera : Camera3D
 {
   [Export] private Petra _petra = null!;
 
-  [Export] private Node3D _nearWallState = null!;
-  [Export] private RayCast3D _gunRay = null!;
-  [Export] private Gun _gun = null!;
-  private Vector3 _gunDefaultPos;
-
   [Export] private Node3D _standLeanPivot = null!;
   [Export] private Node3D _crouchPivot = null!;
   [Export] private Node3D _crouchLeanPivot = null!;
@@ -19,22 +14,11 @@ internal sealed partial class PetraCamera : Camera3D
   [Export] private float _angleLimit = Mathf.Pi / 3f;
 
   [Export] private float _leanSpeed = 10f;
-  private float _pullBackSpeed = 15f;
-
-  [ExportGroup("Weapon Sway")]
-  [Export] private float _swayAmount = 0.5f;
-  [Export] private float _swayThreshold = 0.05f;
-  [Export] private float _swayLerpSpeed = 10f;
-  private Vector2 _mouseRelative;
-
-  private Vector3 _swayOffset;
-  private Vector3 _wallOffset;
 
   public override void _Ready()
   {
     Input.MouseMode = Input.MouseModeEnum.Captured;
     _defaultPos = Position;
-    _gunDefaultPos = _gun.Position;
   }
 
   public override void _UnhandledInput(InputEvent @event)
@@ -49,8 +33,6 @@ internal sealed partial class PetraCamera : Camera3D
     newRotation.Y -= mouseMotion.Relative.X * _mouseSensitivity;
 
     Rotation = newRotation;
-
-    _mouseRelative = mouseMotion.Relative;
   }
 
   public override void _PhysicsProcess(double delta)
@@ -64,11 +46,6 @@ internal sealed partial class PetraCamera : Camera3D
         HandleLeanPos(_defaultPos, _standLeanPivot.Position, delta);
         break;
     }
-
-    UpdateNearWallOffsets(delta);
-    UpdateSwayOffsets(delta);
-
-    _gun.ExtPosOffset = _swayOffset + _wallOffset;
   }
 
   private void HandleLeanPos(Vector3 defaultPos, Vector3 leanPos, double delta)
@@ -91,49 +68,5 @@ internal sealed partial class PetraCamera : Camera3D
     {
       Position = Position.Lerp(to: defaultPos, weight: _leanSpeed * (float)delta);
     }
-  }
-
-  private void UpdateNearWallOffsets(double delta)
-  {
-    Vector3 targetPosOffset = Vector3.Zero, targetOrientation = Vector3.Zero;
-    float maxDist = _gunRay.TargetPosition.Length(); 
-
-    if (_gunRay.IsColliding())
-    {
-      float collisionDist = _gunRay.GetCollisionPoint().DistanceTo(_gunRay.GlobalPosition);
-      float proximity = Mathf.Clamp(1.0f - (collisionDist / maxDist), 0f, 1f);
-      
-      targetPosOffset = proximity * (_nearWallState.Position - _gunDefaultPos);
-      targetOrientation = proximity * _nearWallState.Rotation;
-    }
-
-    // _gun.Position = _gun.Position.Lerp(to: targetPos, weight: _pullBackSpeed * (float)delta);
-    _wallOffset = _wallOffset.Lerp(to: targetPosOffset, weight: _pullBackSpeed * (float)delta);
-    // _gun.Rotation = _gun.Rotation.Lerp(to: targetOrientation, weight: _pullBackSpeed * (float)delta);
-    _gun.ExtRotOffset = _gun.ExtRotOffset.Lerp(to: targetOrientation, weight: _pullBackSpeed * (float)delta);
-  }
-
-  private void UpdateSwayOffsets(double delta)
-  {
-    if (_mouseRelative.LengthSquared() < .5f)
-    {
-      _mouseRelative = Vector2.Zero;
-      return;
-    }
-    else if ((_mouseRelative.X > 0 && _swayOffset.X > 0) || (_mouseRelative.X < 0 && _swayOffset.X < 0))
-      _mouseRelative.X = 0;
-    
-    float targetSwayX = -_mouseRelative.X * _swayAmount;
-    float targetSwayY = _mouseRelative.Y * _swayAmount;
-
-    targetSwayX = Mathf.Clamp(targetSwayX, -_swayThreshold, _swayThreshold);
-    targetSwayY = Mathf.Clamp(targetSwayY, -_swayThreshold, _swayThreshold);
-
-    Vector3 targetSway = new(targetSwayX, targetSwayY, 0);
-
-    // _gun.Position = _gun.Position.Lerp(_gunDefaultPos + swayPos, _swayLerpSpeed * (float)delta);
-    _swayOffset = _swayOffset.Lerp(targetSway, _swayLerpSpeed * (float)delta);
-
-    _mouseRelative *= .2f;
   }
 }
